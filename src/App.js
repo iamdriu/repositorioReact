1f38ba5297db754c285b717e3d160b67d2e98829
Andres <andres.martinez.ccr@gmail.com>
2022-03-15 19:51:35 -0300

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import Avatar from './components/Avatar/Avatar'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import NavBar from './components/NavBar/NavBar';
+import ClassCount from "./components/ClassCount/ClassCount";
+import FunctionCounter from "./components/FunctionCounter/FunctionCounter";
+
+
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <ItemListContainer greeting="Hola Coders"/>
+      <Avatar name="Sebastian" age="28" country="Argentina">
+        <button>Boton</button>
+      </Avatar>
+      <Avatar name="Renzo" age="25" country="Uruguay"> 
+        Texto de prueba
+      </Avatar>
+      <FunctionCounter />
+      <ClassCount />
     </div>
   );
 }
+
+//React.createElement('Avatar', { className: "Avatar" }, [])
 
 export default App;
